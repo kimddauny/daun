2b0afd4b6071b716daf4fe8c75face6e47fa6c7a
stc_155-PC\stc_155 <dauny1005@gmail.com>
2018-05-02 11:07:27 +0900

    LED ON OFF

---
+++ sourcetree_LEDONOFF_0502/sourcetree_LEDONOFF_0502/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * sourcetree_LED_0502.cpp
+ *
+ * Created: 2018-05-02 오전 10:51:59
+ * Author : stc_155
+ */ 
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRD = 0xff;
+	PORTD=0xff;
+	
+    while (1) 
+    {
+		PORTD=0x00;
+		_delay_ms(500);
+		PORTD = 0xff;
+		_delay_ms(500);
+    }
+}
+
